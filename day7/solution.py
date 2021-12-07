e6ac47ebe3c4e4ff3ace26fcf0d2a5a77e09b1d3
Fossan <p.staskiewicz@gmail.com>
2021-12-07 14:32:31 +0100

    lambda code golfing

--- day7/solution.py
+++ day7/solution.py
@@ -5,16 +5,17 @@ from math import floor, ceil
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 data = list(map(int, [line.strip().split(',') for line in open(f'{__location__}/input.txt', 'r')][0]))
 
+fuelSum = lambda progression, round, input: sum(map(lambda crab: progression(abs(crab - round(input))), data))
+
 def solutionA():
     median = statistics.median(data)
-    fuelSum = lambda round: sum(map(lambda crab: abs(crab - round(median)), data))
-    return min(fuelSum(floor), fuelSum(ceil))
+    noProgression = lambda p: p
+    return min(fuelSum(noProgression, floor, median), fuelSum(noProgression, ceil, median))
 
 def solutionB():
     mean = statistics.mean(data)
     fuelProgression = lambda f: f * (f + 1) // 2
-    fuelSum = lambda round: sum(map(lambda crab: fuelProgression(abs(crab - round(mean))), data))
-    return min(fuelSum(floor), fuelSum(ceil))
+    return min(fuelSum(fuelProgression, floor, mean), fuelSum(fuelProgression, ceil, mean))
 
 if __name__ == "__main__":
     print(solutionA())
